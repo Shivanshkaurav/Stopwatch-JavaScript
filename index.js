e3d13b0ffa66296732ab151d2a81308cf43c2b55
Shivansh <shivanshkaurav04@gmail.com>
2024-03-20 18:14:41 +0530

    deploying Stopwatch

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const span =document.querySelector("span");
+const clock = document.querySelector(".clock");
+
+let sec = 0, min=0, hr=0;
+let intervalID;
+
+const startStopwatch = ()=>{
+        intervalID = setInterval(() => {
+        if(sec<59){
+            sec++;
+        }else{
+            sec=0;
+            min++;
+        }
+        if(min>59){
+            hr++;
+            min = 0;
+        }
+
+        const hour = hr.toString().padStart(2,"0");
+        const minute = min.toString().padStart(2,"0");
+        const second = sec.toString().padStart(2,"0");
+        
+        clock.innerHTML = `${hour}:${minute}:${second}`;
+
+    }, 1000);
+}
+const stopStopwatch = ()=>{
+    clearInterval(intervalID);
+}
+
+
+const resetStopwatch = ()=>{
+    location.reload();
+}
